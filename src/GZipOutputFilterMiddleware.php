02456319f5f3a18203dffbf24ce105034bc7e839
Ondřej Koupil <ondra@ondrakoupil.cz>
2019-05-29 12:02:57 +0200

    New middleware added

---
+++ src/GZipOutputFilterMiddleware.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace OndraKoupil\AppTools;
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+use Slim\Http\Stream;
+
+class GZipOutputFilterMiddleware {
+
+	// https://discourse.slimframework.com/t/compressing-text-gzip/2328
+
+	function __invoke(Request $request, Response $response, callable $next) {
+
+		if ($request->hasHeader('Accept-Encoding') &&
+			stristr($request->getHeaderLine('Accept-Encoding'), 'gzip') === false
+		) {
+			// Browser doesn't accept gzip compression
+			return $next($request, $response);
+		}
+
+		/** @var Response $response */
+		$response = $next($request, $response);
+
+		if ($response->hasHeader('Content-Encoding')) {
+			return $next($request, $response);
+		}
+
+		// Compress response data
+		$deflateContext = deflate_init(ZLIB_ENCODING_GZIP);
+		$compressed = deflate_add($deflateContext, (string)$response->getBody(), \ZLIB_FINISH);
+
+		$stream = fopen('php://memory', 'r+');
+		fwrite($stream, $compressed);
+		rewind($stream);
+
+		return $response
+			->withHeader('Content-Encoding', 'gzip')
+			->withHeader('Content-Length', strlen($compressed))
+			->withBody(new Stream($stream));
+
+	}
+
+}
