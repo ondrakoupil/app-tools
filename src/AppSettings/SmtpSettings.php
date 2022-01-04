76355f63fc0f1a0b5caac9e205bf7b59ed29713d
Ondřej Koupil <ondra@ondrakoupil.cz>
2022-01-04 10:54:45 +0100

    SMTP settings

---
+++ src/AppSettings/SmtpSettings.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace OndraKoupil\AppTools\AppSettings;
+
+class SmtpSettings {
+
+	public $host;
+
+	public $username;
+
+	public $password;
+
+	public $port = '465';
+
+	public $secure = 'ssl';
+
+}
