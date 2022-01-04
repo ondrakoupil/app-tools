04f39f8b3537dda657fc03fd565add05c8831b1e
Ondřej Koupil <ondra@ondrakoupil.cz>
2022-01-04 11:56:26 +0100

    LazyCorsMiddleware decoupled from Slim

--- src/LazyCorsMiddleware.php
+++ src/LazyCorsMiddleware.php
@@ -2,21 +2,64 @@
 
 namespace OndraKoupil\AppTools;
 
-use Slim\Http\Request;
-use Slim\Http\Response;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 
+/**
+ * Adds semi-inteligent CORS capabilities.
+ *
+ *
+ * Usage in Slim 4:
+ *
+ * ```
+ * $app->add(
+ *     new LazyCorsMiddleware(
+ *         $app->getResponseFactory(),
+ *         function($request) {
+ *             $routeContext = RouteContext::fromRequest($request);
+ *             $routingResults = $routeContext->getRoutingResults();
+ *             return $routingResults->getAllowedMethods();
+ *         }
+ *     )
+ * );
+ *
+ * // Must be added AFTER LazyCorsMiddleware
+ * $app->addRoutingMiddleware();
+ *
+ * // Add a route for all OPTIONS requests that justs returns the response.
+ * // It is here just for Slim's router to allow OPTIONS requests. The body is not reached since it is short-circuited in the middleware.
+ * $app->options('/{routes:.+}', function ($request, $response, $args) {
+ *   return $response;
+ * });
+ *
+ * ```
+ *
+ * @see https://github.com/slimphp/Slim/issues/2667
+ * @see https://www.slimframework.com/docs/v4/cookbook/enable-cors.html
+ *
+ */
 class LazyCorsMiddleware {
 
-	protected $allowedMethods = array();
+	/**
+	 * @var ResponseFactoryInterface
+	 */
+	private $responseFactory;
 
-	function __construct($allowedMethods = null) {
-		if (!$allowedMethods) {
-			$allowedMethods = array('GET', 'POST');
-		}
-		$this->allowedMethods = $allowedMethods;
+	/**
+	 * @var callable
+	 */
+	private $availableMethodsGetter;
+
+	const DEFAULT_ALLOWED_METHODS = array('GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS');
+
+	function __construct(ResponseFactoryInterface $responseFactory, callable $availableMethodsGetter = null) {
+		$this->responseFactory = $responseFactory;
+		$this->availableMethodsGetter = $availableMethodsGetter;
 	}
 
-	function __invoke(Request $request, Response $response, callable $next) {
+	function __invoke(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface {
 
 		$originHeader = $request->getHeader('Origin');
 		if ($originHeader) {
@@ -28,29 +71,39 @@ class LazyCorsMiddleware {
 		if (preg_match('~localhost~i', $originHeader)) {
 			$allowOrigin = $originHeader;
 		}
-		
-		if ($request->getMethod() === 'OPTIONS') {
-			$newResponse =
-				$response
-					->withStatus(204)
-					->withHeader('Access-Control-Allow-Origin', $allowOrigin)
-					->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-					->withHeader('Access-Control-Allow-Methods', implode(',', $this->allowedMethods));
 
-			return $newResponse;
+		$allowedMethods = $this->evalAllowedMethods($request);
+
+		if ($request->getMethod() === 'OPTIONS') {
+			// Short-circuit
+			$response = $this->responseFactory->createResponse(204);
+			return $this->addHeadersToResponse($response, $allowOrigin, $allowedMethods);
 		}
 
-		/** @var Response $nextResponse */
-		$nextResponse = $next($request, $response);
+		$response = $handler->handle($request);
+		return $this->addHeadersToResponse($response, $allowOrigin, $allowedMethods);
 
-		$newNextResponse =
-			$nextResponse
-				->withHeader('Access-Control-Allow-Origin', $allowOrigin)
-				->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-				->withHeader('Access-Control-Allow-Methods', implode(',', $this->allowedMethods));
+	}
 
-		return $newNextResponse;
+	protected function evalAllowedMethods(ServerRequestInterface $request) {
+		$availableMethodsGetter = $this->availableMethodsGetter;
+		if (!$availableMethodsGetter) {
+			return self::DEFAULT_ALLOWED_METHODS;
+		} else {
+			$allowedMethods = $availableMethodsGetter($request);
+			if (!$allowedMethods and !is_array($allowedMethods)) {
+				return self::DEFAULT_ALLOWED_METHODS;
+			}
+			return $allowedMethods;
+		}
+	}
 
+	protected function addHeadersToResponse(ResponseInterface $response, string $allowOriginHeaderValue, array $allowedMethods): ResponseInterface {
+		return $response
+			->withHeader('Access-Control-Allow-Origin', $allowOriginHeaderValue)
+			->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+			->withHeader('Access-Control-Allow-Methods', implode(',', $allowedMethods))
+		;
 	}
 
 }
