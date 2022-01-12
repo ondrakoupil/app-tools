0f6351f6c17380498d99aa4f4dff2b76c2410788
Ondřej Koupil <ondra@ondrakoupil.cz>
2022-01-12 11:07:27 +0100

    ReaderIterator and ReaderWithCallback

---
+++ src/Importing/Reader/ReaderIterator.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace OndraKoupil\AppTools\Importing\Reader;
+
+use Iterator;
+
+class ReaderIterator implements Iterator {
+
+	/**
+	 * @var ReaderInterface
+	 */
+	protected ReaderInterface $reader;
+
+	/**
+	 * @var mixed
+	 */
+	protected $currentRow = null;
+
+	/**
+	 * @var mixed
+	 */
+	protected $currentRowRead = false;
+
+	/**
+	 *
+	 *
+	 * @param ReaderInterface $reader
+	 */
+	function __construct(ReaderInterface $reader) {
+		$this->reader = $reader;
+	}
+	
+	public function current() {
+		return $this->currentRow;
+	}
+
+	public function next() {
+		$this->currentRowRead = false;
+		$this->readRowIfNeeded();
+	}
+
+	public function key() {
+		return $this->reader->getCurrentPosition();
+	}
+
+	public function valid() {
+		$v = $this->currentRow !== null;
+		if (!$v) {
+			$this->reader->endReading();
+		}
+		return $v;
+	}
+
+	public function rewind() {
+		$this->reader->startReading();
+		$this->readRowIfNeeded();
+	}
+
+	protected function readRowIfNeeded() {
+		if (!$this->currentRowRead) {
+			$this->currentRow = $this->reader->readNextItem();
+			$this->currentRowRead = true;
+		}
+	}
+
+}
