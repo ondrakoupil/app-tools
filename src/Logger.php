32cc2023daac81a70722be798fc22514c8c261a6
Ondřej Koupil <ondra@ondrakoupil.cz>
2024-05-31 09:09:50 +0200

    Improved logger message callbacks

--- src/Logger.php
+++ src/Logger.php
@@ -17,12 +17,30 @@ class Logger extends AbstractLogger {
 
 	protected $runId;
 
+	protected $logCallback;
+
+	protected $logHeaderCallback;
+
+	protected $logExceptionCallback;
+
 	function __construct($logName, $logDirectory, $runId = null, DateTime $today = null) {
 		if (!$today) {
 			$today = new DateTime('now');
 		}
 		$this->filePath = $logDirectory . '/' . $logName . '.' . $today->format('Y-m-d') . '.log';
 		$this->runId = $runId;
+	}
+
+	function setLogCallback(callable $callback): void {
+		$this->logCallback = $callback;
+	}
+
+	function setLogHeaderCallback(callable $callback): void {
+		$this->logHeaderCallback = $callback;
+	}
+
+	public function setLogExceptionCallback(callable $logExceptionCallback): void {
+		$this->logExceptionCallback = $logExceptionCallback;
 	}
 
 	function openIfNeeded() {
@@ -37,7 +55,29 @@ class Logger extends AbstractLogger {
 	public function log($level, $message, array $context = array()) {
 		$this->openIfNeeded();
 		if ($message instanceof Exception) {
-			$message = get_class($message) . " at " . $message->getFile() . " line " . $message->getLine() . "\n" . $message->getMessage() . "\n" . $message->getTraceAsString();
+			$processedException = false;
+			if ($this->logExceptionCallback) {
+				$res = call_user_func_array($this->logExceptionCallback, array($message, $level, $context));
+				if ($res and is_string($res)) {
+					$processedException = true;
+					$message = $res;
+				}
+			}
+			if (!$processedException) {
+				$message = get_class($message) . " at " . $message->getFile() . " line " . $message->getLine() . "\n" . $message->getMessage() . "\n" . $message->getTraceAsString();
+			}
+		}
+		if ($this->logCallback) {
+			$messageReturned = call_user_func_array($this->logCallback, array($message, $level, $context));
+			if ($messageReturned and is_string($messageReturned)) {
+				$message = $messageReturned;
+			}
+		}
+		if ($this->logHeaderCallback) {
+			$header = call_user_func_array($this->logHeaderCallback, array($message, $level, $context));
+			if ($header and is_string($header)) {
+				$message = $header . "\n" . $message;
+			}
 		}
 		$write = date('Y-m-d H:i:s') . ' - ' . ($this->runId ? ($this->runId . ' - ') : '') . $level . ' - ' . $message;
 		fwrite($this->fileHandle, $write . "\n\n");
